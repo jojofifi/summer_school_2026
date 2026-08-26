836a6e19e2554f4af9056989b3e6d249b3fa16da
Joseph Fiorina <josephfiorina.nml@gmail.com>
2026-08-26 13:47:09 +0200

    improve sound analysis

--- src/audio_to_video/sound.py
+++ src/audio_to_video/sound.py
@@ -1,29 +1,29 @@
-from tools.cqt import CQT
-from tools.midi import Midi
-from tools.onset import Onset
-from tools.stft import STFT
-from note import Note
 import librosa
 
+from audio_to_video.note import Note
+from audio_to_video.tools.cqt import CQT
+from audio_to_video.tools.midi import Midi
+from audio_to_video.tools.onset import Onset
+from audio_to_video.tools.stft import STFT
+
+
 class Sound:
-    def __init__(self, filename: str) -> None :
+    def __init__(self, filename: str) -> None:
         self.filename = filename
         self.y, self.sr = librosa.load(filename, sr=None)
         self.hop_length = 512
         print("Sound loaded")
 
-    def startAnalyse(self, exportFilename) -> None :
+    def startAnalyse(self, exportFilename) -> None:
         print("Analyse start :")
-        stft = STFT.getSTFT(self.y)
-        print("\tSTFT genereted")
         cqt = CQT.getCQT(self.y, self.sr, self.hop_length)
         CQT.exportCQT(cqt, self.sr, "/home/joseph/Downloads/Trumpet_CQT.png")
         print("\tCQT genereted")
         onset_times = Onset.detectOnset(self.y, self.sr)
+        # onset_times = Onset.detectOnset2(stft)
         print("\tOnset detected")
-        notes = Note.getNotes(cqt, onset_times, self.sr, self.hop_length, 12*7, 12)
+        notes = Note.getNotes(cqt, onset_times, self.sr, self.hop_length, 12 * 7, 12)
 
         print(exportFilename)
-        print(librosa.frames_to_time(onset_times, sr=self.sr, hop_length=self.hop_length))
         midi = Midi()
         midi.createFile(exportFilename, notes)
