1d7938e47de04574380d53d4a8d4ede3a29c8cce
Joseph Fiorina <josephfiorina.nml@gmail.com>
2026-08-31 15:58:27 +0200

    improve test multi-instruments

--- src/audio_to_video/test.py
+++ src/audio_to_video/test.py
@@ -2,9 +2,8 @@ import pretty_midi as pm
 from collections import defaultdict
 
 
-class Test():
-
-    def __init__(self, midiFileCorrect: str, midiFileTest: str) -> None :
+class Test:
+    def __init__(self, midiFileCorrect: str, midiFileTest: str) -> None:
         self.midiDataCorrect = pm.PrettyMIDI(midiFileCorrect)
         self.midiDataTest = pm.PrettyMIDI(midiFileTest)
 
@@ -14,79 +13,131 @@ class Test():
             for note in instrument.notes:
                 cntCorrect += 1
 
-        cntTest= 0
+        cntTest = 0
         for instrument in self.midiDataTest.instruments:
             for note in instrument.notes:
                 cntTest += 1
 
-        percentage = 100 - (abs(cntTest-cntCorrect)/((cntTest+cntCorrect)/2)*50)
+        percentage = 100 - (
+            abs(cntTest - cntCorrect) / ((cntTest + cntCorrect) / 2) * 50
+        )
 
         print("Correct number of Notes: " + str(cntCorrect))
         print("Guessed number of Notes: " + str(cntTest))
-        print("Global Percentage difference: "+ str(percentage))
-
+        print("Global Percentage difference: " + str(percentage))
 
     def compareNotes(self):
-        all_notes = defaultdict(list)
+        all_notes = []
+        all_notes_test = []
 
         for instrument in self.midiDataCorrect.instruments:
             for note in instrument.notes:
-                all_notes[instrument.name].append({
-                    'pitch': note.pitch,
-                    'start': round(note.start, 1),
-                    'end': round(note.start, 1),
-                    'verified': False
-                })
+                all_notes.append(
+                    {
+                        "pitch": note.pitch,
+                        "start": round(note.start, 1),
+                        "end": round(note.start, 1),
+                        "verified": False,
+                    }
+                )
 
         for instrument in self.midiDataTest.instruments:
             for note in instrument.notes:
-                for everyNote in all_notes[instrument.name]:
-                    if everyNote['pitch'] == note.pitch and everyNote['start'] == round(note.start, 1) and everyNote['end'] == round(note.start, 1):
-                        everyNote['verified'] = True
+                all_notes_test.append(
+                    {
+                        "pitch": note.pitch,
+                        "start": round(note.start, 1),
+                        "end": round(note.start, 1),
+                    }
+                )
+
+        for correct_note in all_notes:
+            for test_note in all_notes_test:
+                if (
+                    correct_note["pitch"] == test_note["pitch"]
+                    and correct_note["start"] == test_note["start"]
+                    and correct_note["end"] == test_note["end"]
+                ):
+                    correct_note["verified"] = True
 
         countCorrect = 0
         countFalse = 0
 
-        for everyInstrument in all_notes:
-            for everyNote in all_notes[everyInstrument]:
-                if  everyNote['verified'] == True:
-                    countCorrect += 1
-                else:
-                    countFalse += 1
+        for correct_note in all_notes:
+            if correct_note["verified"] == True:
+                countCorrect += 1
+            else:
+                countFalse += 1
 
-        percentage = 100 - (abs(countCorrect - countFalse) / ((countCorrect + countFalse) / 2)*50)
+        if countFalse == 0:
+            percentage = 100
+        else:
+            percentage = 100 - (
+                abs(countCorrect) / ((countCorrect + countFalse) / 2) * 50
+            )
 
         print("Correctly Guessed number of Notes: " + str(countCorrect))
         print("wrongly Guessed number of Notes: " + str(countFalse))
         print("Global Percentage difference: " + str(percentage))
 
-    
-    def compareNbrInstrument(self):
+    def compareByInstrument(self):
         instruments_correct = {}
-        instruments_test = {}
 
         for instrument in self.midiDataCorrect.instruments:
             name = pm.program_to_instrument_name(instrument.program)
-            instruments_correct[name] = len(instrument.notes)
+            instruments_correct[name] = []
 
+            for note in instrument.notes:
+                instruments_correct[name].append(
+                    {
+                        "pitch": note.pitch,
+                        "start": round(note.start, 1),
+                        "end": round(note.start, 1),
+                        "verified": False,
+                    }
+                )
+
+        count_not_found = 0
+        count_found = 0
+        count_nonexistent = 0
         for instrument in self.midiDataTest.instruments:
             name = pm.program_to_instrument_name(instrument.program)
-            instruments_test[name] = len(instrument.notes)
+            # print(name)
+            # print(instruments_correct[name])
 
-        all_instrument_names = set(instruments_correct.keys()) | set(
-            instruments_test.keys()
-        )
+            for note in instrument.notes:
+                is_found = False
+                for correct_note in instruments_correct[name]:
+                    is_correct_pitch = correct_note["pitch"] == note.pitch
+                    is_correct_start = correct_note["start"] == round(note.start, 1)
 
-        for name in all_instrument_names:
-            cntCorrect = instruments_correct.get(name, 0)
-            cntTest = instruments_test.get(name, 0)
+                    if is_correct_pitch and is_correct_start:
+                        correct_note["verified"] = True
+                        is_found = True
 
-            print(f"Instrument: {name}")
+                if not is_found:
+                    count_nonexistent += 1
 
-            if cntTest + cntCorrect == 0:
-                percentage = 100
-            else:
-                avg = (cntTest + cntCorrect) / 2
-                percentage = 100 - (abs(cntTest - cntCorrect) / avg * 50)
+        for instrument, notes in instruments_correct.items():
+            count_correct = 0
+            count_false = 0
+            for note in notes:
+                if note["verified"]:
+                    count_correct += 1
+                    count_found += 1
+                else:
+                    count_false += 1
+                    count_not_found += 1
 
+            print(f"Instrument: {instrument}")
+            print(f"  Correct notes: {count_correct}")
+            print(f"  False notes: {count_false}")
+            percentage = abs(count_correct) / (count_correct + count_false) * 100
             print(f"  Similarity: {percentage:.2f}%")
+
+        print(f"Total notes found: {count_nonexistent + count_found}")
+        print(f"Total correct notes: {count_found}")
+        print(f"Notes were not in original audio: {count_nonexistent}")
+        print(
+            f"Total Similarity: {(count_found / (count_found + count_not_found) * 100):.2f}%"
+        )
