d3997d4ba2e447dd4a5ae47f36911de20c9ddfd4
AmineF-collab <ferchichiamine2007@gmail.com>
2026-09-01 15:49:57 +0200

    rename file and put on folder for clarity

--- src/audio_to_video/object/sunandmoon.py
+++ src/audio_to_video/object/sunandmoon.py
@@ -1,7 +1,35 @@
-import pygame
-import drawmethod
+import drawingtools.drawmethod as drawmethod
 import math
 
+
+class Lune():
+    def __init__(self,display,x,y,time):
+        self.x = x
+        self.y = y
+        self.time = time
+        self.display = display
+        self.angle = 22.2
+        self.distance = 450
+        self.acceleration = 2*self.distance/math.pow(self.time,2)
+        self.initial_speed = self.acceleration * (self.time/2) / math.sin(math.radians(self.angle))
+        self.gravity = 2*self.distance/math.pow(self.time,2)
+        self.end = False
+        self.timer = 0
+        self.rayon = 40
+    def move(self):
+        self.speedx = self.initial_speed*math.cos(math.radians(self.angle))
+        self.speedy = (self.gravity)*self.timer+self.initial_speed*-math.sin(math.radians(self.angle))
+        self.x += self.speedx*1/90
+        self.y += self.speedy*1/90
+        self.timer += 1/90
+        if self.timer >= self.time:
+            self.end = True
+            self.x = 1050
+            self.y = 200
+
+    def draw(self,color2):
+        drawmethod.drawcircle(self.display,(142,142,142),self.x,self.y,self.rayon,15)
+        drawmethod.drawcircle(self.display,color2,self.x-30,self.y,self.rayon,15)
 
 class Soleil():
     def __init__(self,display,x,y,time):
@@ -16,7 +44,7 @@ class Soleil():
         self.gravity = 2*self.distance/math.pow(self.time,2)
         self.end = False
         self.timer = 0
-        self.rayon = 30
+        self.rayon = 40
         self.nbr_point = 10
         self.color = (255, 215, 0)
     def move(self):
@@ -33,5 +61,6 @@ class Soleil():
         drawmethod.draw_rays(self.display,self.color,self.x,self.y,self.rayon,self.nbr_point)
     
 
+        
         
         
