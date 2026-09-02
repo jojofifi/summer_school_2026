a185f20fb4564586c57c3df4bbb808a39b9abec4
Guillaume Hyvert <guillaume.hyvert@ik.me>
2026-09-02 12:37:34 +0200

    ui final

--- src/audio_to_video/lancement.py
+++ src/audio_to_video/lancement.py
@@ -80,7 +80,7 @@ sun = object.sunandmoon.Soleil(display,-50,200,2*(duree_totale/5))
 
 
 START_COORDINATES = (0,screen_height/2)
-tiles = Tiles(START_COORDINATES, display, screen_height, screen_width, 130)
+tiles = Tiles(START_COORDINATES, display, screen_height, screen_width, 125)
 mapTile = tiles.createTiles(45,3)
 
 while run:
