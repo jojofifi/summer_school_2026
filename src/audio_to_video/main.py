f954b17a7cdd1b3b44fdcaca8e2cc4c0aec07113
Guillaume Hyvert <guillaume.hyvert@ik.me>
2026-09-02 11:09:59 +0200

    color for mountains

--- src/audio_to_video/main.py
+++ src/audio_to_video/main.py
@@ -1,5 +1,5 @@
 from sound import Sound
-
+#from isoTest import Render
 def main() -> None:
     print("Audio to video started")
     
@@ -7,10 +7,10 @@ def main() -> None:
     # exportFilename = "./src/audio_to_video/media/PinkPanther_Piano_Only.mid"
     # filename = "./src/audio_to_video/media/PinkPanther_Trumpet_Only.mp3"
     # exportFilename = "./src/audio_to_video/media/PinkPanther_Trumpet_Only.mid"
-    filename = "./src/audio_to_video/media/PinkPanther_Both.mp3"
-    exportFilename = "./src/audio_to_video/media/PinkPanther_Both.mid"
-    sound = Sound(filename)
-    sound.startAnalyse(exportFilename)
+    #filename = "./src/audio_to_video/media/PinkPanther_Both.mp3"
+    #exportFilename = "./src/audio_to_video/media/PinkPanther_Both.mid"
+    #sound = Sound(filename)
+    #sound.startAnalyse(exportFilename)
 
 if __name__ == "__main__":
     main()
